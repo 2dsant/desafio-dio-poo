65c5efa013de33121c23432f3c189718473be601
da Silva Santos <ddds@gft.com>
2023-07-31 22:50:05 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPOO.Models;
+
+Nokia c1 = new Nokia("985412544", "Nokia", "11111", 64);
+Iphone c2 = new Iphone("952147585", "Apple", "22222", 124);
+
+c1.Ligar();
+c1.InstalarAplicativo("Facebook");
+
+Console.WriteLine("======");
+c2.Ligar();
+c2.InstalarAplicativo("Facebook");
